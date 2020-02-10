1d271c612fca1f2e410befde3353a3817d7fc226
Louis Vaslin <louis.vaslin@clermont.in2p3.fr>
2020-02-10 17:41:44 +0000

    Upload New File

---
+++ test.py
@@ -0,0 +1,63 @@
+# Here we test pyBumpHunter.
+# The result will be compared to what can be obtained with the original C++ version.
+# We will use histograms ranging between 0 and 20 with 60 even bins.
+
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import pyBumpHunter as BH
+from datetime import datetime  ## Used to compute the execution time
+import uproot as upr  ## Used to read data from a root file
+
+
+# Open the file
+File = upr.open('data.root')
+File.items()
+
+# Background
+bkg = File['bkg'].arrays(outputtype=np.array)
+
+# Data
+data = File['data'].arrays(outputtype=np.array)
+
+# Position of the bump in the data
+Lth = 5.5
+
+# Range for the hitograms (same that the one used with C++ BumpHunter)
+rang = [0,20]
+
+# Plot the 2 distributions
+F = plt.figure(figsize=(12,8))
+plt.title('Test distribution')
+plt.hist((bkg,data),bins=60,histtype='step',range=rang,label=('bakground','data'))
+plt.legend()
+plt.savefig('results_py/hist.png',bbox_inches='tight')
+plt.close(F)
+
+
+# Call the BumpHunter function
+begin = datetime.now()
+BH.BumpHunter(
+    data,bkg,Rang=rang,
+    Width_min=2,
+    Width_max=6,
+    Width_step=1,
+    Scan_step=1,
+    npe=800,
+    NWorker=4
+)
+end = datetime.now()
+print('time={}'.format(end-begin))
+print('')
+
+BH.GetTomography(data,filename='results_py/tomography.png')
+
+BH.PrintBumpInfo()
+BH.PrintBumpTrue(data)
+
+BH.PlotBump(data,bkg,filename='results_py/bump.png')
+
+BH.PlotBHstat(show_Pval=True,filename='results_py/BH_statistics.png')
+
+
