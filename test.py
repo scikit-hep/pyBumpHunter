56e4ac76497b09615777b5ee632eba35aae5cd02
Louis Vaslin <louis.vaslin@clermont.in2p3.fr>
2020-04-24 09:19:50 +0000

    Added example for signal injection use.

--- test.py
+++ test.py
@@ -21,6 +21,9 @@ bkg = File['bkg'].arrays(outputtype=np.array)
 # Data
 data = File['data'].arrays(outputtype=np.array)
 
+# Signal
+sig = File['sig'].arrays(outputtype=np.array)
+
 # Position of the bump in the data
 Lth = 5.5
 
@@ -30,14 +33,35 @@ rang = [0,20]
 # Plot the 2 distributions
 F = plt.figure(figsize=(12,8))
 plt.title('Test distribution')
-plt.hist((bkg,data),bins=60,histtype='step',range=rang,label=('bakground','data'))
+plt.hist((bkg,data),bins=60,histtype='step',range=rang,label=('bakground','data'),linewidth=2)
 plt.legend()
 plt.savefig('results_py/hist.png',bbox_inches='tight')
 plt.close(F)
-
-
+'''
+pval_py = np.fromfile('pval_py.txt',sep='\n')
+pval_c = np.fromfile('gpval_c/pval_new.txt',sep='\n')
+F = plt.figure(figsize=(12,8))
+plt.hist((pval_py,pval_c),bins=40,histtype='step',linewidth=2,label=('python','c++'))
+plt.legend(fontsize='large')
+plt.xlabel('Global p-value',size='large')
+plt.savefig('gpval_all/pval_new.png',bbox_inches='tight')
+plt.close(F)
+f = open('gpval_all/stat_new.txt','w')
+print('stat python : ',file=f)
+print(f'   mean={pval_py.mean()}',file=f)
+print(f'   std={pval_py.std()}',file=f)
+print(f'   min={pval_py.min()}',file=f)
+print(f'   max={pval_py.max()}',file=f)
+print('',file=f)
+print('stat c++ : ',file=f)
+print(f'   mean={pval_c.mean()}',file=f)
+print(f'   std={pval_c.std()}',file=f)
+print(f'   min={pval_c.min()}',file=f)
+print(f'   max={pval_c.max()}',file=f)
+f.close()
+'''
 # Call the BumpHunter function
-print('###FIRST CALL###')
+print('####BmupHunter call####')
 begin = datetime.now()
 BH.BumpHunter(
     data,bkg,Rang=rang,
@@ -45,7 +69,7 @@ BH.BumpHunter(
     Width_max=6,
     Width_step=1,
     Scan_step=1,
-    npe=800,
+    npe=10000,
     NWorker=1,
     Seed=666
 )
@@ -71,33 +95,42 @@ BH.PlotBump(data,bkg,filename='results_py/bump1.png')
 # Get and save statistics plot
 BH.PlotBHstat(show_Pval=True,filename='results_py/BH_statistics1.png')
 
+print('')
+# Set injection parrameter and call SignalInject function with keepparam argument
+# (so we keep the same BumpHunter parameters, except NPE that I also modify)
+BH.sigma_limit = 3
+BH.str_min = -1
+#BH.str_step = 0.1
+BH.str_scale = 'log'
+BH.signal_exp = 150
 
-# Change Parameters and cal BumpHunter function with keepparam argument
-BH.scan_step='half'
-BH.seed=42
-print('###SECOND CALL###')
+print('####SignalInject call####')
 begin = datetime.now()
-BH.BumpHunter(data,bkg,keepparam=True)
+BH.SignalInject(sig,bkg,is_hist=False,keepparam=True)
 end = datetime.now()
 print('time={}'.format(end-begin))
 print('')
 
-# Print new bump
+# Print new bump after signal injection (use BH.data_inject to get the generated data)
 BH.PrintBumpInfo()
-BH.PrintBumpTrue(data,bkg)
+BH.PrintBumpTrue(BH.data_inject,bkg)
 print('   mean (true) = {}'.format(Lth))
 print('')
 
+Hbkg = np.histogram(bkg,bins=BH.bins,weights=BH.weights)[0]
 
 # Get and save new tomography plot
-BH.GetTomography(data,filename='results_py/tomography2.png')
+BH.GetTomography(BH.data_inject,is_hist=True,filename='results_py/tomography2.png')
 
 
 # Get and save new bump plot
-BH.PlotBump(data,bkg,filename='results_py/bump2.png')
+BH.PlotBump(BH.data_inject,Hbkg,is_hist=True,filename='results_py/bump2.png')
 
 
 # Get and save new statistics plot
 BH.PlotBHstat(show_Pval=True,filename='results_py/BH_statistics2.png')
+
+# Get and save the injection plot
+BH.PlotInject(filename=('results_py/SignalInject.png','results_py/SignalInject_log.png'))
 
 
