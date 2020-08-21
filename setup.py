8a23af9fe7e2ec373a2124530ffa71764f102e15
Louis VASLIN <64971347+lovaslin@users.noreply.github.com>
2020-08-21 16:24:59 +0200

    New attempt to fix the versioning scheme.

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup
 import setuptools_scm
 import toml
 
+def local_scheme(version):
+    return ""
+
 setup(
-    use_scm_version=True,
+    use_scm_version={"local_scheme": local_scheme},
 )
