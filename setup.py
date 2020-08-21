b4a5ea7805a624d655ae306118bce75d64bdecb4
Louis VASLIN <64971347+lovaslin@users.noreply.github.com>
2020-08-21 15:51:26 +0200

    Revert the last changes in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,5 @@ import setuptools_scm
 import toml
 
 setup(
-    use_scm_version={'version_scheme': 'final-release',
-                     'local_scheme': 'final-release'},
+    use_scm_version=True,
 )
